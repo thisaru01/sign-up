d78eb76129f2423c4b8042d16243fa400a72eac9
Thisaru Rajapaksha <thisarumiuranga@gmail.com>
2024-12-20 11:04:22 +0530

    responsive v1

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,22 +3,17 @@ import "../App";
 import backgroundImage from "../assets/background.jpg";
 import logo1 from "../assets/logo1.png";
 import logo2 from "../assets/logo2.png";
-import { IoPersonCircleOutline } from "react-icons/io5";
-import { IoEyeOutline } from "react-icons/io5";
-import { IoLockClosedOutline } from "react-icons/io5";
-import { IoMailOutline } from "react-icons/io5";
-import { IoCallOutline } from "react-icons/io5";
-import { IoLogoGoogle } from "react-icons/io5";
+import { IoPersonCircleOutline, IoEyeOutline, IoLockClosedOutline, IoMailOutline, IoCallOutline, IoLogoGoogle } from "react-icons/io5";
 
 function SignUp() {
   return (
     <div
-      className="flex items-center justify-start h-screen bg-cover bg-center  font-regular"
+      className="flex items-center justify-center md:justify-start h-screen md:h-screen bg-cover bg-center font-regular"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="bg-white shadow-md rounded-lg w-[45%] h-[95%] p-6 flex flex-col items-center ml-5">
+      <div className="bg-white shadow-md rounded-lg  sm:w-[75%] md:w-[45%] md:h-[95%] sm:h-[95%] h-[95%]  p-6 flex flex-col items-center mx-auto md:ml-5">
         <img src={logo1} alt="Logo1" className="w-10 h-10 mb-0.9" />
-        <img src={logo2} alt="Logo1" className="w-40 h-400 mb-0" />
+        <img src={logo2} alt="Logo2" className="w-40 h-40 mb-0" />
 
         <h1 className="text-2xl font-semibold mb-2 text-center">
           Ready to Learn?
@@ -29,23 +24,19 @@ function SignUp() {
         </p>
 
         <button className="flex items-center border border-gray-300 text-black py-1 px-12 rounded-lg w-auto mb-6">
-        <IoLogoGoogle className="text-2xl mx-2 my-1 text-gray-600" />
+          <IoLogoGoogle className="text-2xl mx-2 my-1 text-gray-600" />
           Sign up with Google
         </button>
 
-        <div className="flex items-center justify-center mb-6 w-[450px]">
+        <div className="flex items-center justify-center mb-6 w-full">
           <div className="flex-grow border-t-[3px] border-[#c6c5c6]"></div>
           <p className="mx-4 text-black">or</p>
           <div className="flex-grow border-t-[3px] border-[#c6c5c6]"></div>
         </div>
 
-
-        <form className="w-[75%] flex flex-col items-center gap-2">
-          
-          <div className="flex items-center w-[75%] py-[2px] px-6 mb-3 border border-gray-300 rounded-lg p-2">
-
+        <form className="w-[75%] sm:w-[75%] md:h-[75%] flex flex-col items-center gap-2">
+          <div className="flex items-center w-full py-[2px] px-6 mb-3 border border-gray-300 rounded-lg p-2">
             <IoPersonCircleOutline className="text-gray-400 m-2 text-2xl" />
-
             <input
               type="text"
               placeholder="Username"
@@ -53,23 +44,18 @@ function SignUp() {
             />
           </div>
 
-          <div className="flex items-center w-[75%] py-[2px] px-6 mb-3 border border-gray-300 rounded-lg p-2 relative">
-             
-              <IoLockClosedOutline className="text-gray-400 m-2 text-2xl" />
-
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full outline-none"
-              />
-
-              <IoEyeOutline className="text-gray-400 mr-2 text-2xl cursor-pointer absolute right-2" />
+          <div className="flex items-center w-full py-[2px] px-6 mb-3 border border-gray-300 rounded-lg p-2 relative">
+            <IoLockClosedOutline className="text-gray-400 m-2 text-2xl" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full outline-none"
+            />
+            <IoEyeOutline className="text-gray-400 mr-2 text-2xl cursor-pointer absolute right-2" />
           </div>
 
-          <div className="flex items-center w-[75%] py-[2px] px-6 mb-3 border border-gray-300 rounded-lg p-2">
-
+          <div className="flex items-center w-full py-[2px] px-6 mb-3 border border-gray-300 rounded-lg p-2">
             <IoMailOutline className="text-gray-400 m-2 text-2xl" />
-
             <input
               type="email"
               placeholder="Email Address"
@@ -77,7 +63,7 @@ function SignUp() {
             />
           </div>
 
-          <div className="flex items-center w-[75%] py-[2px] px-6 mb-5 border border-gray-300 rounded-lg p-2">
+          <div className="flex items-center w-full py-[2px] px-6 mb-5 border border-gray-300 rounded-lg p-2">
 
             <IoCallOutline className="text-gray-400 m-2 text-2xl" />
 
